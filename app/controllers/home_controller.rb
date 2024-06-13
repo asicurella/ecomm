fceecb0e5f244fcb60e3bd77d3ed9c0b42838b35
aliyasicurella <aliyajsicurella@gmail.com>
2024-06-13 18:31:22 -0400

    initial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+class HomeController < StoreController
+  helper 'spree/products'
+  respond_to :html
+
+  def index
+    # Fetch specific taxons as needed
+    @basic_bundle_taxon = Spree::Taxon.find_by(permalink: 'categories/basic-bundle')
+    @caps_taxon = Spree::Taxon.find_by(permalink: 'categories/clothing/caps')
+
+    if @basic_bundle_taxon
+      basic_bundle_searcher = build_searcher(params.merge(include_images: true, taxon: @basic_bundle_taxon.id))
+      @french_tip_products = basic_bundle_searcher.retrieve_products
+    else
+      @french_tip_products = []
+    end
+
+    if @caps_taxon
+      caps_searcher = build_searcher(params.merge(include_images: true, taxon: @caps_taxon.id))
+      @caps_products = caps_searcher.retrieve_products
+    else
+      @caps_products = []
+    end
+
+    # Fetch all taxonomies with their root and children
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+    # Original Spree logic to fetch products
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+  end
+end
